ac894e4de54d99e057a3ae543d71a72f6e9f3b69
AJDev1 <aj@gmail.com>
2018-06-11 00:14:45 +0530

    msg5

--- GitHub/src/com/nt/test/GitMessage.java
+++ GitHub/src/com/nt/test/GitMessage.java
@@ -4,7 +4,7 @@ public class GitMessage {
 
 	public static void main(String[] args) {
 		System.out.println("hello Welcome to Github Testing");
-		System.out.println("SSSSS111");
+		System.out.println("SSSSS111222333");
 		
 
 	}
